76ebf45e060789138eebe23257208d418d712fbd
Jiahao Xiang <jiahaoxiang2000@gmail.com>
2024-09-25 13:18:21 +0800

    only show the image component

--- miniprogram/page/component/index.js
+++ miniprogram/page/component/index.js
@@ -30,51 +30,11 @@ Page({
 
   data: {
     list: [
-      {
-        id: 'view',
-        name: '视图容器',
-        open: false,
-        pages: ['view', 'scroll-view', 'swiper', 'movable-view', 'cover-view']
-      }, {
-        id: 'content',
-        name: '基础内容',
-        open: false,
-        pages: ['text', 'icon', 'progress', 'rich-text']
-      }, {
-        id: 'form',
-        name: '表单组件',
-        open: false,
-        pages: ['button', 'checkbox', 'form', 'input', 'label', 'picker', 'picker-view', 'radio', 'slider', 'switch', 'textarea', 'editor']
-      }, {
-        id: 'nav',
-        name: '导航',
-        open: false,
-        pages: ['navigator']
-      }, {
+     {
         id: 'media',
         name: '媒体组件',
-        open: false,
-        pages: ['image', 'video', 'camera', 'live-pusher', 'live-player']
-      }, {
-        id: 'map',
-        name: '地图',
-        open: false,
-        pages: ['map', { appid: 'wxe3f314db2e921db0', name: '腾讯位置服务示例中心'}]
-      }, {
-        id: 'canvas',
-        name: '画布',
-        open: false,
-        pages: ['canvas-2d', 'webgl']
-      }, {
-        id: 'open',
-        name: '开放能力',
-        open: false,
-        pages: ['ad', 'open-data', 'web-view']
-      }, {
-        id: 'obstacle-free',
-        name: '无障碍访问',
-        open: false,
-        pages: ['aria-component']
+        open: true,
+        pages: ['image']
       }
     ],
     theme: 'light'
@@ -105,6 +65,5 @@ Page({
     this.setData({
       list
     })
-    wx.reportAnalytics('click_view_programmatically', {})
   }
 })

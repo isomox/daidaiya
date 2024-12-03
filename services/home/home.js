9111830ceb16c1b2817559109d33b02f5a106b3b
Jiahao Xiang <jiahaoxiang2000@gmail.com>
2024-12-03 16:11:48 +0800

    home category

--- services/home/home.js
+++ services/home/home.js
@@ -9,32 +9,20 @@ function mockFetchHome() {
       swiper: genSwiperImageList(),
       tabList: [
         {
-          text: '精选推荐',
-          key: 0,
+          text: "Coding",
+          key: "coding",
         },
         {
-          text: '夏日防晒',
-          key: 1,
+          text: "Video",
+          key: "video",
         },
         {
-          text: '二胎大作战',
-          key: 2,
+          text: "Music",
+          key: "music",
         },
         {
-          text: '人气榜',
-          key: 3,
-        },
-        {
-          text: '好评榜',
-          key: 4,
-        },
-        {
-          text: 'RTX 30',
-          key: 5,
-        },
-        {
-          text: '手机也疯狂',
-          key: 6,
+          text: "LLVM",
+          key: "llvm",
         },
       ],
       activityImg: `${cdnBase}/activity/banner.png`,
